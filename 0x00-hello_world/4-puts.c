9a07af3408d3501ef4627035e2927606aa0a7eef
vagrant <vagrant@vagrant.vm>
2021-02-03 18:02:11 +0000

    fixing minor spacing issues and betty warnings

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/*
- * Main - Where the program starts
- * Return - Should always be 0
+/**
+ * main - Where the program starts
+ * Return: Should always be 0
  */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,16 @@
 #include<stdio.h>
-/*
-*Main - Where the program starts
-*Return - Should always be 0
-*/
+/**
+ * main - Where the program starts
+ * Return: Should always be 0
+ */
 int main(void)
 {
 
-/* sizeof evaluates the size of a variable*/
 printf("Size of char: %zu byte(s)\n", sizeof(char));
 printf("Size of int: %zu bytes(s)\n", sizeof(int));
 printf("Size of long int: %zu byte(s)\n", sizeof(long int));
 printf("Size of long long int: %zu byte(s)\n", sizeof(long long int));
 printf("Size of float: %zu byte(s)\n", sizeof(float));
 
- return (0);
+return (0);
 }
