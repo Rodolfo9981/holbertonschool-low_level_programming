dcf8bd801e848900cb53522b23fff13d4105ad42
vagrant <vagrant@vagrant.vm>
2021-02-03 16:20:02 +0000

    adding in 0x00-hello_world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * Main - Where the program starts
+ * Return - Should always be 0
+ */
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return (0);
+}
