6129dfeec367ba3e3dd143816410f1b98a4d2cc8
vagrant <vagrant@vagrant.vm>
2021-02-04 18:16:43 +0000

    fixing betty errrors

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,27 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* main - generates a random number to n and last find the last digit
- * elseif statement is to see if the last digit is 0, greater than 5, or less than 6 and not 0
- * Return: Should always be 0
- */
+/*
+* main - generates a random number to n and last find the last digit
+* elseif is to see if last digit is 0 greater than 5 or less than 6 and not 0
+* Return: Should always be 0
+*/
 int main(void)
 {
-  int n, last;
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
-  last = (n % 10);
-    if (last < 6 && last != 0)
-    {
-      printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
-    }
-  else if (last > 5)
-    {
-      printf("Last digit of %d is %d and is greater than 5\n", n, last);
-    }
-  else
-    {
-      printf("Last digit of %d is %d and is 0\n", n, last);
-    }
-  return (0);
+int n, last;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+last = (n % 10);
+if (last < 6 && last != 0)
+{
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
+}
+else if (last > 5)
+{
+printf("Last digit of %d is %d and is greater than 5\n", n, last);
+}
+else
+{
+printf("Last digit of %d is %d and is 0\n", n, last);
+}
+return (0);
 }
